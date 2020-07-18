9d23fc60e454eef8832dbbfd9824e0d65d3241ef
daniel2231 <kangheon2231@gmail.com>
2020-07-18 21:30:06 +0900

    changed project file

--- Project/router.js
+++ Project/router.js
@@ -1,13 +1,16 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-const viewPath = './views/';
+const viewPath = "./views/";
 
-router.get('/', (req, res) => {
-    res.sendFile(viewPath + 'index.html', { root: __dirname });
+router.get("/", (req, res) => {
+  res.sendFile(viewPath + "index.html", { root: __dirname });
 });
 
-router.get(`/${id}`, (req, res) => {
-    res.sendFile(viewPath + `${id}.html`, {root: __dirname});
-})
+let restaurants = [""];
+restaurants.forEach(function (name) {
+  app.get(name, function (req, res) {
+    res.sendFile(viewPath + `/restaurants/${name}.html`), { root: __dirname };
+  });
+});
 
 module.exports = router;
